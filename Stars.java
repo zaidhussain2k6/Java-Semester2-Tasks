5f7b3f9bd3607ab68152f59cd6e4c4f2ef4d0a93
Zaid Hussain <zaidhussain2k6@gmail.com>
2025-09-19 20:04:13 +0500

    Update

---
+++ Stars.java
@@ -0,0 +1,14 @@
+public class Stars {
+    public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("          ");
+        System.out.println("*        *");
+        System.out.println("          ");
+        System.out.println("*        *");
+        System.out.println("          ");
+        System.out.println("**********");
+    }
+    
+
+
+}
